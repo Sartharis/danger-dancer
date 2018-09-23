8b02742f883f625bfee99714698ea76318ad5dd8
Sartharis <jan.orlowsk@gmail.com>
2018-09-23 11:05:49 -0400

    Beat Manager + Songs

---
+++ DANGER DANCER/Assets/BeatEffects.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatEffects : MonoBehaviour
+{
+
+    private SpriteEffects effects;
+
+	// Use this for initialization
+	void Start ()
+    {
+        effects = GetComponent<SpriteEffects>();
+	    BeatManager.Instance.OnBeat += OnBeat;
+	}
+	
+    void OnBeat()
+    {
+        effects.deformX = 0.05f;
+        effects.deformY = 0.05f;
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
