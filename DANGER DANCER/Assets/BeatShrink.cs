43979106c7241b48ecefe5fd6dcea48cdcec4756
Hefei <franktu99@hotmail.com>
2018-09-23 15:52:35 -0400

    BeatShrink
    Shrink the postzone based on beats

---
+++ DANGER DANCER/Assets/BeatShrink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatShrink : MonoBehaviour {
+
+    [SerializeField] private int beatstoShrink = 10;
+    float scalar = 0;
+    // Use this for initialization
+    void Start () {
+        scalar = -transform.localScale.x / beatstoShrink;
+        BeatManager.Instance.OnBeat += OnBeat;
+    }
+
+    void OnBeat() {
+
+        transform.localScale += new Vector3(scalar, scalar, 0);
+
+        if (transform.localScale.x <= 0){
+            Destroy(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
