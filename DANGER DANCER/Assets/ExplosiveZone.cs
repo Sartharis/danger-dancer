8c326c3bfdbb9bf69030b47477b4d1dced825689
Hefei <franktu99@hotmail.com>
2018-10-07 16:07:37 -0400

    Added ExplosiveZone
    Player can access the explosive zone when spinning, otherwise the player will fall

---
+++ DANGER DANCER/Assets/ExplosiveZone.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveZone : MonoBehaviour {
+
+
+    [SerializeField]
+    private float spinSpeed = 1000f;
+    [SerializeField]
+    private float destroyTime = 0.5f;
+    [SerializeField]
+    private float acceleration = -2000f;
+    private bool spin = false;
+    private SpriteEffects effects;
+
+    private void Start()
+    {
+        effects = GetComponent<SpriteEffects>();
+    }
+
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        PlayerDancer dancer = other.GetComponent<PlayerDancer>();
+        if (dancer)
+        {
+            if (dancer.actionState == EActionState.AS_IDLE || dancer.actionState == EActionState.AS_POSE)
+            {
+                dancer.Fall(new Vector2(transform.position.x - dancer.transform.position.x, transform.position.y - dancer.transform.position.y));
+                Destroy(gameObject, destroyTime);
+            }
+        }
+
+    }
+    public void Update()
+    {
+
+    }
+}
